182d5c9bdd93e8d93bbb742283955111b57119e1
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-07-06 16:08:21 -0300

    🥉 find_largest_palindrome_substring method is working

--- main.py
+++ main.py
@@ -24,11 +24,20 @@ class Desafios:
         print(f"{' ' * (len(self.title) - 1)}: {string_without_duplicates}")
         return string_without_duplicates
 
-    def find_largest_palindrome(self, string_phrase: str) -> str:
+    def find_largest_palindrome_substring(self, string_phrase: str) -> str:
+        """ outputs only the palindromes and the largest one that is the substring that will be returned
+        :param string_phrase: any string text
+        :return: the largest palindrome in the string_phrase passed by parameter if it exists, else an empty string
+        """
         words = string_phrase.split()
-        words_in_reverse = [w[-1::-1] for w in words]
-        return 'init debug'
+        words_list_in_reverse = [word[-1::-1] for word in words]
+        palindrome_words = [word for e, word in enumerate(words_list_in_reverse) if word.upper() == words[e].upper()]
+        if palindrome_words:
+            largest_palindrome_substring = max(palindrome_words)
+            return largest_palindrome_substring
 
+        # palindrome_words list could be empty
+        return ''
 
 if __name__ == '__main__':
     desafio = Desafios()
@@ -37,7 +46,8 @@ if __name__ == '__main__':
     desafio_02 = desafio.remove_duplicated_characters('Hello, World!')
     desafio_02_extra = desafio.remove_duplicated_characters('Sem caracteres duplicados!')
 
-    desafio_03 = desafio.find_largest_palindrome('bababab')
+    # desafio_03 = desafio.find_largest_palindrome('hello world bababab lalalal, bababab natannatan')
+    desafio_03 = desafio.find_largest_palindrome_substring('hello world anna natan')
 
     print('debuging')
 
