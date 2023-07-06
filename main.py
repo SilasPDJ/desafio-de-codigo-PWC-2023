bf0ca1b03ed399142597c32631190a71bb00bb54
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-07-06 17:38:17 -0300

    🥉 finding palindromes before finding the largest one

--- main.py
+++ main.py
@@ -32,14 +32,30 @@ class Desafios:
         :param string_phrase: any string text
         :return: the largest palindrome in the string_phrase passed by parameter if it exists, else an empty string
         """
-        words = string_phrase.split()
-        words_list_in_reverse = [word[-1::-1] for word in words]
-        palindrome_words = [word for e, word in enumerate(words_list_in_reverse) if word.upper() == words[e].upper()]
+        words_list = string_phrase.split()
+        words_list_in_reverse = [word[-1::-1] for word in words_list]
+        palindrome_words = [word for e, word in enumerate(words_list_in_reverse) if word.upper() == words_list[e].upper()]
 
-        print("Ex.3)")
+        # finding palindrome in any place of a word
+        palindrome_word_found_list = {}
+        for word in words_list:
+
+            # The minimum number of letters possible for palindromes is 3
+            if len(word) < 3:
+                continue
+
+            # find palindromes
+            palindrome_word_found_list[word] = []
+            for start_index in range(len(word)):
+                # i + 3 because the minimum number of letters possible for palindromes is 3
+                for end_index in range(start_index + 3, len(word)+1):
+                    term = word[start_index:end_index]
+                    if term == term[::-1] and term not in palindrome_word_found_list:
+                        palindrome_word_found_list[word].append(term)
+
         if palindrome_words:
+            print("Ex.3) Find Largest palindrome word")
             largest_palindrome_substring = max(palindrome_words)
-
             print(f"{self.title} {palindrome_words}")
             print(f"{' ' * (len(self.title) - 1)}: {largest_palindrome_substring}")
 
@@ -56,8 +72,9 @@ if __name__ == '__main__':
     desafio_02 = desafio.remove_duplicated_characters('Hello, World!')
     desafio_02_extra = desafio.remove_duplicated_characters('Sem caracteres duplicados!')
 
-    # desafio_03 = desafio.find_largest_palindrome('hello world bababab lalalal, bababab natannatan')
-    desafio_03 = desafio.find_largest_palindrome_substring('hello world anna natan')
+    desafio_03_extra_1 = desafio.find_largest_palindrome_substring('babaddalalalana')
+    desafio_03 = desafio.find_largest_palindrome_substring('babad')
+    desafio_03_extra_2 = desafio.find_largest_palindrome_substring('hello world anna natan')
 
     print('debuging')
 
