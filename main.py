7dd9e11923fbc8ce2885afeb67b88fda20a0a15b
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-07-06 15:45:33 -0300

    🎉 2nd's finished & let's go

--- main.py
+++ main.py
@@ -13,10 +13,24 @@ class Desafios:
 
         return revered_phrase
 
+    def remove_duplicated_characters(self, string_phrase: str):
+        """ outputs string_phrase as it is and reversed, finally returns it without duplicated characters
+        :param string_phrase: any string text
+        :return: string_without_duplicates
+        """
+        string_without_duplicates = ''.join(dict.fromkeys(string_phrase).keys())
+
+        print(f"{self.title} {string_phrase}")
+        print(f"{' ' * (len(self.title) - 1)}: {string_without_duplicates}")
+        return string_without_duplicates
+
 
 if __name__ == '__main__':
     desafio = Desafios()
     desafio_01 = desafio.reverse_phrase_part_1('Hello, World! OpenAI is amazing!')
+
+    desafio_02 = desafio.remove_duplicated_characters('Hello, World!')
+    desafio_02_extra = desafio.remove_duplicated_characters('Sem caracteres duplicados!')
 
     print('debuging')
 
