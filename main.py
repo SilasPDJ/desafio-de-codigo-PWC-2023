bbeb285f3572208df50fdeaf72b0ba5a797ad914
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-07-07 15:42:24 -0300

    🎉🥈 Exercise 5 is close to be done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 from itertools import permutations
 import requests
+from typing import List
 
 
 class Desafios:
@@ -73,7 +74,7 @@ class Desafios:
         return ''
 
     def capitalize_phrasal_strings(self, string_phrase: str) -> str:
-        """
+        """ outputs the param and the return
         :param string_phrase: any string text
         :return: string phrase capitalized when it starts a new sentence
         """
@@ -90,104 +91,152 @@ class Desafios:
 
         return capitalized_string_phrase
 
-    def is_anagram_of_palindrome(self, word: str) -> list:
-        print("Ex.5) Get Anagrams of palindromes")
+    def is_anagram_of_palindrome(self, word: str) -> bool:
+        """ outputs when if it's an anagram of palindrome, or only anagram or only palindrome
+        :param word:
+        :return bool: if True -> is an anagram of palindrome. else False
+        """
+        print(f"Ex.5) Get Anagrams of palindromes for word \033[1;34m{word}\033[m")
 
         # get anagrams
-        anagrams_dict = self.get_anagrams_dict(main_word=word)
+        anagrams_dict = self._get_anagrams_dict(main_word=word)
+        # load a txt dictionary from the web
+        portuguese_real_words, english_real_words = self._loads_pt_and_en_words_dictonary()
         anagrams_of_palindrome_words = []
 
         # All palindromes are anagrams as well
         if self._is_palindrome(word):
-            if word[-1::-1].lower() in self._loads_portuguese_dictonary_as_list():
+            # racecar case
+            anagram = word[-1::-1].lower()
+            if anagram in portuguese_real_words or anagram in english_real_words:
                 anagrams_of_palindrome_words.append(word[-1::-1].lower())
 
         for key, values in anagrams_dict.items():
             # check if any anagram is a palindrome
             for anagram in values:
-                if self._is_palindrome(anagram):
+                if self._is_palindrome(anagram) and anagram not in anagrams_of_palindrome_words:
                     anagrams_of_palindrome_words.append(anagram)
         if any(anagrams_of_palindrome_words):
             # Yes it is anagram of palindrome
-            print(f"The anagrams of palindrome for {word} are: {anagrams_of_palindrome_words}")
-            return anagrams_of_palindrome_words
+            print("\033[1;32mYes!!! \033[m", end='')
+            if len(anagrams_of_palindrome_words) == 1:
+                print(f"The anagram of palindrome is: {anagrams_of_palindrome_words[0]}")
+            else:
+                # maybe it is not really possible to display this block code
+                print(f"The anagrams of palindrome are: {anagrams_of_palindrome_words}")
+            return True
         elif anagrams_dict:
             # Despite having anagrams, it doesn't contains palindrome words
-            print(f"Despite having anagrams like {list(anagrams_dict.values())[0]}, It DOESN'T have any anagrams of palindrome")
-            return []
+            print(
+                f"Despite having anagrams like {list(anagrams_dict.values())[0]}, It DOESN'T have any anagram of palindrome")
+            return False
         else:
             # Nor a palindrome neither an anagram
             print("We don't have any palindromes or anagrams")
-            return []
+            return False
 
-    def get_anagrams_dict(self, main_word: str) -> dict:
+    def _get_anagrams_dict(self, main_word: str) -> dict:
+        """
+        :param main_word: any single word
+        :return: dict containing each unique letter in main_word
+        """
         if len(main_word.split()) > 1:
-            pass
-            # TODO search for more than one word in language dictionary and load english dictionary as well
+            raise ValueError('It is supposed to be only one word per string')
         # Get dictionary words
-        dictionary_as_list = self._loads_portuguese_dictonary_as_list()
+        portuguese_real_words, english_real_words = self._loads_pt_and_en_words_dictonary()
         # Get all sequence possible through word using permutation
-        permuted_sequence = self._get_permutation_word_sequence(word=main_word)
+        permuted_sequence = self._get_permutation_word_sequence(any_word=main_word)
 
         dict_with_words_by_initial = {}
-
-        # loop through all main_word letters  that did not repeat yet
-        for e, initial_letter in enumerate(sorted(set(letter.lower() for letter in main_word))):
-            dict_with_words_by_initial[initial_letter] = []
-            # filter in dictionary by the initial letter of the real word to check if term exists
-            real_words = list(filter(lambda w: w.startswith(initial_letter), dictionary_as_list))
-            # filter only terms that contain the initial letter
-            for term in filter(lambda w: w.startswith(initial_letter), permuted_sequence):
-                # # an anagram needs to be an existent and real word
-                if term in real_words:
-                    dict_with_words_by_initial[initial_letter].append(term)
+        # looping through both english and portuguese dictionary real words
+        for dictionary_as_list in [portuguese_real_words, english_real_words]:
+            # loop through all main_word letters  that did not repeat yet
+            for e, initial_letter in enumerate(sorted(set(letter.lower() for letter in main_word))):
+                dict_with_words_by_initial[initial_letter] = []
+                # filter in dictionary by the initial letter of the real word to check if term exists
+                real_words = list(filter(lambda w: w.startswith(initial_letter), dictionary_as_list))
+                # filter only terms that contain the initial letter
+                for term in filter(lambda w: w.startswith(initial_letter), permuted_sequence):
+                    # # an anagram needs to be an existent and real word
+                    if term in real_words:
+                        dict_with_words_by_initial[initial_letter].append(term)
 
             # print(f'5 - Debugging finished for words that starts with the letter of main_word: {initial_letter} in position: {e}')
         return dict_with_words_by_initial
 
-    def _is_palindrome(self, any_word: str):
+    def _is_palindrome(self, any_word: str) -> bool:
+        """
+        :return: if any_word is palindrome or not
+        """
         return any_word == any_word[-1::-1]
 
-    def _get_permutation_word_sequence(self, word: str, custom_lower_case=True):
-        # Gera todas as permutações possíveis da sequência
-        list_of_sequences_possible = list(permutations(word))
+    def _get_permutation_word_sequence(self, any_word: str, custom_lower_case=True) -> list:
+        """
+        :param any_word: any single word
+        :param custom_lower_case: True by default. It is better to validate if the custom_lower_case argument is always True
+        :return: All sequences possible of any_word
+        """
+        # Generate all permutations (all sequences possible for each letter of a word)
+        list_of_sequences_possible = list(permutations(any_word))
 
-        # Junta as letras de cada permutação em uma palavra
+        # Creates each word joining each sequence generated
         words = [''.join(sequenced_word) for sequenced_word in list_of_sequences_possible]
+
+        #
         if custom_lower_case:
             return [w.lower() for w in words]
         else:
             return words
 
-    def _loads_portuguese_dictonary_as_list(self) -> list:
+    def _loads_pt_and_en_words_dictonary(self) -> List[List[str]]:
+        """ If the files don't exist yet, it requests the contents and save them
+        :return: This method loads two lists containing portuguese and english words from dictionary.
+        """
+        destination_files = ["br-utf8.txt", 'en-words.txt']
 
-        destination_file = "br-utf8.txt"
-        try:
-            with open(destination_file, 'r') as file:
-                return [word.replace('\n', '') for word in file.readlines()]
-        except FileNotFoundError:
-            url = "https://www.ime.usp.br/~pf/dicios/br-utf8.txt"
-            response = requests.get(url)
-            content = response.content.decode("utf-8")
+        urls = ["https://www.ime.usp.br/~pf/dicios/br-utf8.txt",
+                "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt"]
+        en_with_pt_lists = []
 
-            with open(destination_file, "w") as file:
-                file.write(content)
-            return content.split('\n')
+        for destination_file, url in zip(destination_files, urls):
+            try:
+                with open(destination_file, 'r') as file:
+                    en_with_pt_lists.append([word.replace('\n', '') for word in file.readlines()])
+
+            except FileNotFoundError:
+                response = requests.get(url)
+                content = response.content.decode("utf-8")
+                content_list = content.split('\n')
+
+                if destination_file == 'en-words.txt':
+                    # omg, the racecar word is not yet on this txt
+                    content_list.insert(content_list.index('racecard'), 'racecar')
+
+                en_with_pt_lists.append(content_list)
+
+                with open(destination_file, "w") as file:
+                    file.write("\n".join(content_list))
+
+        return en_with_pt_lists
 
 
 if __name__ == '__main__':
     desafio = Desafios()
     desafio_01 = desafio.reverse_phrase_part_1('Hello, World! OpenAI is amazing!')
+    print()
 
     desafio_02 = desafio.remove_duplicated_characters('Hello, World!')
     desafio_02_extra = desafio.remove_duplicated_characters('Sem caracteres duplicados!')
+    print()
 
     desafio_03 = desafio.find_largest_palindrome_substring('babad')
     desafio_03_extra_1 = desafio.find_largest_palindrome_substring('babaddalalalana ana')
     desafio_03_extra_2 = desafio.find_largest_palindrome_substring('hello world ana ama')
+    print()
 
     desafio_04 = desafio.capitalize_phrasal_strings("hello. how are you? i'm fine, thank you")
+    print()
 
     desafio_05 = desafio.is_anagram_of_palindrome('racecar')
-    desafio_05_extra = desafio.is_anagram_of_palindrome('Alegria')
+    desafio_05_extra_1 = desafio.is_anagram_of_palindrome('Alegria')
     print('debuging')
